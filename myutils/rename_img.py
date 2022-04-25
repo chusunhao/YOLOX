ea0091ab9dc5eba7eff141c82b10953c39476760
sstc <Chu1220@sjtu.edu.cn>
2022-04-26 02:39:17 +0800

    modify transform module

---
+++ myutils/rename_img.py
@@ -0,0 +1,21 @@
+import os
+
+# path为批量文件的文件夹的路径
+root_path = './spark_dataset/train_rgb'
+new_path = './spark_dataset/images/train/'
+# root_path = './spark_dataset/validate_rgb'
+# new_path = './spark_dataset/images/val/'
+
+for sat_name in os.listdir(root_path):
+
+    path = os.path.join(root_path, sat_name)
+    # 文件夹中所有文件的文件名
+    file_names = os.listdir(path)
+
+    print(len(file_names))
+    # 外循环遍历所有文件名，内循环遍历每个文件名的每个字符
+    for name in file_names:
+        index = name.split('_')[1]
+        new_name = sat_name + '_' + index + '_img.png'
+        os.renames(os.path.join(path, name), os.path.join(new_path, new_name))
+        print(f"image {index} Done!")
